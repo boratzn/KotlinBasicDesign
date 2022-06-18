c045de0026ed45d8873138c4465dd96aea7ce454
Bora TÜZÜN <boratzn94@gmail.com>
2022-06-18 19:26:46 +0300

    Tasarım ödevi tamamlandı.

---
+++ app/src/main/java/com/boratuzun/tasarimodev/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.boratuzun.tasarimodev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
